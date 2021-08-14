450ceee6b4c65ce04b65f0bcfaac6a19efa64c3f
EduardoEbbers <cel.ebbers@gmail.com>
2021-08-14 11:50:02 -0300

    Ocorrencia MVC Completo

--- live/src/main/java/com/dio/live/controller/OcorrenciaController.java
+++ live/src/main/java/com/dio/live/controller/OcorrenciaController.java
@@ -1,13 +1,123 @@
 package com.dio.live.controller;
 
+import com.dio.live.model.Ocorrencia;
 import com.dio.live.service.OcorrenciaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/ocorrencia")
 public class OcorrenciaController {
     @Autowired
     private OcorrenciaService ocorrenciaService;
+
+    @PostMapping
+    public ResponseEntity<Ocorrencia> createOcorrencia(@RequestBody Ocorrencia ocorrencia) {
+        try {
+            if((ocorrencia.getIdOcorrencia() == null) || (ocorrencia.getNome() == null) || (ocorrencia.getDescricao() == null)) {
+                throw new Error("Ocorrência Id, Nome e Descrição são Obrigatórios!");
+            }
+            if(ocorrencia.getIdOcorrencia() <= 0) {
+                throw new Error("Ocorrencia Id está incorreto!");
+            }
+            return new ResponseEntity<>(
+                    ocorrenciaService.create(ocorrencia),
+                    HttpStatus.CREATED);
+        } catch(Error e) {
+            return new ResponseEntity(
+                    e.getMessage(),
+                    HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Ocorrencia>> getAllOcorrencia() {
+        try {
+            return new ResponseEntity<>(
+                    ocorrenciaService.findAll(),
+                    HttpStatus.OK);
+        } catch(Error e) {
+            return new ResponseEntity(
+                    e.getMessage(),
+                    HttpStatus.BAD_REQUEST);
+        } catch(NoSuchElementException e) {
+            return new ResponseEntity(
+                    e.getMessage(),
+                    HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/{idOcorrencia}")
+    public ResponseEntity<Ocorrencia> getOcorrenciaById(@PathVariable("idOcorrencia") Long idOcorrencia) {
+        try {
+            if(idOcorrencia == null) {
+                throw new Error("Ocorrência Id é Obrigatório!");
+            }
+            if(idOcorrencia <= 0) {
+                throw new Error("Ocorrência Id está incorreto!");
+            }
+            return new ResponseEntity<>(
+                    ocorrenciaService.findById(idOcorrencia),
+                    HttpStatus.OK);
+        } catch(Error e) {
+            return new ResponseEntity(
+                    e.getMessage(),
+                    HttpStatus.BAD_REQUEST);
+        } catch(NoSuchElementException e1) {
+            return new ResponseEntity(
+                    e1.getMessage(),
+                    HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping
+    public ResponseEntity<Ocorrencia> updateOcorrencia(@RequestBody Ocorrencia ocorrencia) {
+        try {
+            if((ocorrencia.getIdOcorrencia() == null) || (ocorrencia.getNome() == null) || (ocorrencia.getDescricao() == null)) {
+                throw new Error("Ocorrência Id, Nome e Descrição são Obrigatórios!");
+            }
+            if(ocorrencia.getIdOcorrencia() <= 0) {
+                throw new Error("Ocorrência Id está incorreto!");
+            }
+            return new ResponseEntity<>(
+                    ocorrenciaService.update(ocorrencia),
+                    HttpStatus.OK
+            );
+        } catch(Error e) {
+            return new ResponseEntity(
+                    e.getMessage(),
+                    HttpStatus.BAD_REQUEST);
+        } catch(NoSuchElementException e1) {
+            return new ResponseEntity(
+                    e1.getMessage(),
+                    HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{idOcorrencia}")
+    public ResponseEntity deleteOcorrencia(@PathVariable("idOcorrencia") Long idOcorrencia) {
+        try {
+            if(idOcorrencia == null) {
+                throw new Error("Ocorrência Id é Obrigatório!");
+            }
+            if(idOcorrencia <= 0) {
+                throw new Error("Ocorrência Id está incorreto!");
+            }
+            ocorrenciaService.delete(idOcorrencia);
+            return new ResponseEntity(HttpStatus.NO_CONTENT);
+        } catch(Error e) {
+            return new ResponseEntity(
+                    e.getMessage(),
+                    HttpStatus.BAD_REQUEST);
+        } catch(NoSuchElementException e1) {
+            return new ResponseEntity(
+                    e1.getMessage(),
+                    HttpStatus.NOT_FOUND);
+        }
+    }
 }
--- live/src/main/java/com/dio/live/service/OcorrenciaService.java
+++ live/src/main/java/com/dio/live/service/OcorrenciaService.java
@@ -1,11 +1,68 @@
 package com.dio.live.service;
 
+import com.dio.live.model.Ocorrencia;
 import com.dio.live.repository.OcorrenciaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @Service
 public class OcorrenciaService {
     @Autowired
     private OcorrenciaRepository ocorrenciaRepository;
+
+    public Ocorrencia create(Ocorrencia ocorrencia) {
+        try {
+            Optional<Ocorrencia> ocorr = ocorrenciaRepository.findById(ocorrencia.getIdOcorrencia());
+            if(ocorr.isPresent()) {
+                throw new Error("Ocorrência já existe!");
+            }
+            return ocorrenciaRepository.save(ocorrencia);
+        } catch(Error e) {
+            throw new Error(e.getMessage());
+        }
+    }
+
+    public List<Ocorrencia> findAll() {
+        try {
+            return ocorrenciaRepository.findAll();
+        } catch (Error e) {
+            throw new Error(e.getMessage());
+        }
+    }
+
+    public Ocorrencia findById(Long id) {
+        try {
+            return ocorrenciaRepository
+                    .findById(id)
+                    .orElseThrow(() -> new NoSuchElementException("Ocorrência não existe!"));
+        } catch(Error e) {
+            throw new Error(e.getMessage());
+        }
+    }
+
+    public Ocorrencia update(Ocorrencia ocorrencia) {
+        try {
+            ocorrenciaRepository
+                    .findById(ocorrencia.getIdOcorrencia())
+                    .orElseThrow(() -> new NoSuchElementException("Ocorrência não existe!"));
+            return ocorrenciaRepository.save(ocorrencia);
+        } catch(Error e) {
+            throw new Error(e.getMessage());
+        }
+    }
+
+    public void delete(Long id) {
+        try {
+            ocorrenciaRepository
+                    .findById(id)
+                    .orElseThrow(() -> new NoSuchElementException("Ocorrência não existe!"));
+            ocorrenciaRepository.deleteById(id);
+        } catch(Error e) {
+            throw new Error(e.getMessage());
+        }
+    }
 }
